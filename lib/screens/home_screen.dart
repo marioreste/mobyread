369e400627eb2ce8f3645853b0b2979cdcdde4fa
marioreste <mariogallozzi@gmail.com>
2025-10-22 15:27:01 +0200

    Added home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen(this.switchScreen, {super.key});
+  final Function() switchScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image:  AssetImage('assets/images/mobyread_logo.png')),
+          const SizedBox(height: 30),
+          const Text(
+            'Benvenuto in MobyRead!',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 40),
+          OutlinedButton.icon(
+            onPressed: switchScreen('reading'),
+            icon: const Icon(Icons.book),
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              side: const BorderSide(color: Colors.white),
+              foregroundColor: Colors.black,
+            ),
+            label: const Text('Vai ai libri da leggere'),
+          ),
+        ],
+      )
+    );
+  }
+}
